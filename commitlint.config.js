24d8fd13bf1d751f68df196bd487e726079687e2
jorduino <jordanhabash@gmail.com>
2026-01-30 19:19:03 -0500

    chore: fix formatting

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,7 +3,7 @@
  */
 const config = {
 	extends: ["@commitlint/config-conventional"],
-	ignores: [(message) => message.startsWith("Initial commit")],
+	ignores: [message => message.startsWith("Initial commit")],
 	rules: {
 		"subject-case": [0, "always", ["sentence-case", "start-case", "pascal-case", "upper-case"]],
 	},
